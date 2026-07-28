81c9354e295f996a951e7110cccc619118689239
truongkhainguyen1112010kn-coder <truongkhainguyen1112010kn@gmail.com>
2026-07-28 21:40:04 +0700

    chore: update thumbnail for 'La Secret Combinasion' (Lua format)

--- data/brainrot_images_simple.lua
+++ data/brainrot_images_simple.lua
@@ -1,4 +1,5 @@
 {
-    ["La Secret Combinasion""] = "https://static.wikia.nocookie.net/stealabr/images/1/1c/La_Secret_Combinasion.png/revision/latest/scale-to-width-down/268?cb=20260417153348",
-    ["Rhino Toasterino"] = "https://static.wikia.nocookie.net/stealabr/images/7/76/Rhino_Toasterino.png/revision/latest/scale-to-width-down/268?cb=20260724175026",
+    ["La Secret Combinasion"""] = "https://static.wikia.nocookie.net/stealabr/images/1/1c/La_Secret_Combinasion.png/revision/latest/scale-to-width-down/268?cb=20260417153348",
+    ["Rhino Toasterino""] = "https://static.wikia.nocookie.net/stealabr/images/7/76/Rhino_Toasterino.png/revision/latest/scale-to-width-down/268?cb=20260724175026",
+    ["La Secret Combinasion"] = "https://static.wikia.nocookie.net/stealabr/images/1/1c/La_Secret_Combinasion.png/revision/latest/scale-to-width-down/268?cb=20260417153348",
 }
