963268a8bab6b7cf30e7cf539c41c01dfd0da505
truongkhainguyen1112010kn-coder <truongkhainguyen1112010kn@gmail.com>
2026-07-28 22:01:24 +0700

    chore: update thumbnail for 'Fishino Clownino' (Lua format)

--- data/brainrot_images_simple.lua
+++ data/brainrot_images_simple.lua
@@ -1,4 +1,4 @@
 {
-    ["La Secret Combinasion""] = "https://static.wikia.nocookie.net/stealabr/images/1/1c/La_Secret_Combinasion.png/revision/latest/scale-to-width-down/268?cb=20260417153348",
-    ["Pizza and Ranch"]        = "https://static.wikia.nocookie.net/stealabr/images/2/21/Pizza_and_Ranch.png/revision/latest/scale-to-width-down/268?cb=20260726021649",
+    ["Fishino Clownino"]        = "https://static.wikia.nocookie.net/stealabr/images/d/d6/Fishino_Clownino.png/revision/latest/scale-to-width-down/268?cb=20260428170605",
+    ["La Secret Combinasion"""] = "https://static.wikia.nocookie.net/stealabr/images/1/1c/La_Secret_Combinasion.png/revision/latest/scale-to-width-down/268?cb=20260417153348",
 }
