fd5b96dd42ed2e1bbeb8ac44d8c6dad5dd7a24e4
truongkhainguyen1112010kn-coder <truongkhainguyen1112010kn@gmail.com>
2026-07-28 22:23:11 +0700

    chore: update thumbnail for 'Los Admins' (Lua format)

--- data/brainrot_images_simple.lua
+++ data/brainrot_images_simple.lua
@@ -1,4 +1,4 @@
 {
-    ["Los Admins"]          = "https://static.wikia.nocookie.net/stealabr/images/d/d8/Los_Admins.png/revision/latest/scale-to-width-down/268?cb=20260711200015",
-    ["Rubiko and Kubiko"""] = "https://static.wikia.nocookie.net/stealabr/images/1/13/Rubiko_and_Kubiko.png/revision/latest/scale-to-width-down/268?cb=20260718211149",
+    ["Los Admins"]           = "https://static.wikia.nocookie.net/stealabr/images/d/d8/Los_Admins.png/revision/latest/scale-to-width-down/268?cb=20260711200015",
+    ["Rubiko and Kubiko""""] = "https://static.wikia.nocookie.net/stealabr/images/1/13/Rubiko_and_Kubiko.png/revision/latest/scale-to-width-down/268?cb=20260718211149",
 }
