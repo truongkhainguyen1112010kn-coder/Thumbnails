3043552ccac95b2071994ddc4af7f42189dd4c99
truongkhainguyen1112010kn-coder <truongkhainguyen1112010kn@gmail.com>
2026-07-28 22:16:21 +0700

    Update brainrot_images_simple.lua

--- data/brainrot_images_simple.lua
+++ data/brainrot_images_simple.lua
@@ -1,4 +1,1 @@
-{
-    ["Examen Bros"]       = "https://static.wikia.nocookie.net/stealabr/images/c/ca/Examen_Bros.png/revision/latest/scale-to-width-down/268?cb=20260725203412",
-    ["Rubiko and Kubiko"] = "https://static.wikia.nocookie.net/stealabr/images/1/13/Rubiko_and_Kubiko.png/revision/latest/scale-to-width-down/268?cb=20260718211149",
-}
+
