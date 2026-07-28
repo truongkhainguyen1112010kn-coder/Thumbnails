ded89c948e00bba1f27c80ae3559f2d5004ed1eb
truongkhainguyen1112010kn-coder <truongkhainguyen1112010kn@gmail.com>
2026-07-28 22:14:35 +0700

    chore: update thumbnail for 'Rubiko and Kubiko' (Lua format)

--- data/brainrot_images_simple.lua
+++ data/brainrot_images_simple.lua
@@ -1,4 +1,4 @@
 {
-    ["Examen Bros"]           = "https://static.wikia.nocookie.net/stealabr/images/c/ca/Examen_Bros.png/revision/latest/scale-to-width-down/268?cb=20260725203412",
-    ["La Secret Combinasion"] = "https://static.wikia.nocookie.net/stealabr/images/1/1c/La_Secret_Combinasion.png/revision/latest/scale-to-width-down/268?cb=20260417153348",
+    ["La Secret Combinasion""] = "https://static.wikia.nocookie.net/stealabr/images/1/1c/La_Secret_Combinasion.png/revision/latest/scale-to-width-down/268?cb=20260417153348",
+    ["Rubiko and Kubiko"]      = "https://static.wikia.nocookie.net/stealabr/images/1/13/Rubiko_and_Kubiko.png/revision/latest/scale-to-width-down/268?cb=20260718211149",
 }
