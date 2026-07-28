522af6de5ba3b5f9e9c023e2289052349f9cf3ba
truongkhainguyen1112010kn-coder <truongkhainguyen1112010kn@gmail.com>
2026-07-28 21:53:19 +0700

    Update brainrot_images_simple.lua

--- data/brainrot_images_simple.lua
+++ data/brainrot_images_simple.lua
@@ -1,4 +1,1 @@
-{
-    ["La Secret Combinasion"]  = "https://static.wikia.nocookie.net/stealabr/images/1/1c/La_Secret_Combinasion.png/revision/latest/scale-to-width-down/268?cb=20260417153348",
-    ["La Secret Combinasion""] = "https://static.wikia.nocookie.net/stealabr/images/1/1c/La_Secret_Combinasion.png/revision/latest/scale-to-width-down/268?cb=20260417153348",
-}
+
