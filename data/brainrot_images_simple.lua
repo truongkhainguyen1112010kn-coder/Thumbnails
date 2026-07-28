a275aa6e6aa19af4848bd4167d4c6738f7453fa7
truongkhainguyen1112010kn-coder <truongkhainguyen1112010kn@gmail.com>
2026-07-28 22:06:36 +0700

    chore: update thumbnail for 'Examen Bros' (Lua format)

--- data/brainrot_images_simple.lua
+++ data/brainrot_images_simple.lua
@@ -1,4 +1,4 @@
 {
-    ["Fishino Clownino"]        = "https://static.wikia.nocookie.net/stealabr/images/d/d6/Fishino_Clownino.png/revision/latest/scale-to-width-down/268?cb=20260428170605",
-    ["La Secret Combinasion"""] = "https://static.wikia.nocookie.net/stealabr/images/1/1c/La_Secret_Combinasion.png/revision/latest/scale-to-width-down/268?cb=20260417153348",
+    ["Examen Bros"]              = "https://static.wikia.nocookie.net/stealabr/images/c/ca/Examen_Bros.png/revision/latest/scale-to-width-down/268?cb=20260725203412",
+    ["La Secret Combinasion""""] = "https://static.wikia.nocookie.net/stealabr/images/1/1c/La_Secret_Combinasion.png/revision/latest/scale-to-width-down/268?cb=20260417153348",
 }
